51a2e60807df66dcc338e5c45afd9fe9dd3ad6d6
Raikish <albert.montagut.casero@gmail.com>
2022-04-17 15:35:31 +0200

    feat(EnemySpawn): GetSpawnPoint test failing

--- Assets/_Project/Code/Tests/EnemyGeneratorTests.cs
+++ Assets/_Project/Code/Tests/EnemyGeneratorTests.cs
@@ -1,5 +1,6 @@
 ﻿using NSubstitute;
 using NUnit.Framework;
+using UnityEngine;
 
 namespace GraveKiller
 {
@@ -25,7 +26,54 @@ namespace GraveKiller
         [Test]
         public void GetSpawnPoint()
         {
-            Assert.Fail();
+            var mapStartCoordinates = 0;
+            var squaredMapSize = 20;
+            var docPlayerPosition = new Vector3(5, 0, 0);
+            var docMapSize = new Vector3(squaredMapSize, 0, squaredMapSize);
+
+            var docSquaredSpawnZone = new SquaredSpawnZone(
+                new SignedRange(new Range(5, 10)),
+                new SignedRange(new Range(10, 12)));
+
+            var sut = new EnemyGenerator(Mathf.Infinity, null);
+            var spawnPoint = sut.GetSpawnPoint();
+
+            var spawnPointXPositionIsInsideMap = spawnPoint.x >= mapStartCoordinates &&
+                                                 spawnPoint.x <= squaredMapSize;
+
+            var spawnPointYPositionIsInsideMap = spawnPoint.z >= mapStartCoordinates &&
+                                                 spawnPoint.z <= squaredMapSize;
+
+            Assert.That(spawnPointXPositionIsInsideMap);
+            Assert.That(spawnPointYPositionIsInsideMap);
+
+            var correctRangeMinPositiveX = docPlayerPosition.x + 5;
+            var correctRangeMaxPositiveX = docPlayerPosition.x + 10;
+            var correctRangeMinNegativeX = -correctRangeMaxPositiveX;
+            var correctRangeMaxNegativeX = -correctRangeMinPositiveX;
+
+            var correctRangeMinPositiveY = docPlayerPosition.z + 10;
+            var correctRangeMaxPositiveY = docPlayerPosition.z + 12;
+            var correctRangeMinNegativeY = -correctRangeMaxPositiveY;
+            var correctRangeMaxNegativeY = -correctRangeMinPositiveY;
+
+            var isXInTheCorrectRange =
+                (spawnPoint.x >= correctRangeMinPositiveX &&
+                 spawnPoint.x <= correctRangeMaxPositiveX) ||
+                (spawnPoint.x >= correctRangeMinNegativeX &&
+                 spawnPoint.x <= correctRangeMaxNegativeX);
+
+            var isYInTheCorrectRange =
+                (spawnPoint.z >= correctRangeMinPositiveY &&
+                 spawnPoint.z <= correctRangeMaxPositiveY) ||
+                (spawnPoint.z >= correctRangeMinNegativeY &&
+                 spawnPoint.z <= correctRangeMaxNegativeY);
+
+            Assert.That(isXInTheCorrectRange,
+                $"x = {spawnPoint.x}, expected >={correctRangeMinPositiveX} <= {correctRangeMaxPositiveX} >= {correctRangeMinNegativeX} <= {correctRangeMaxNegativeX}");
+
+            Assert.That(isYInTheCorrectRange,
+                $"y = {spawnPoint.z}, expected >={correctRangeMinPositiveY} <= {correctRangeMaxPositiveY} >= {correctRangeMinNegativeY} <= {correctRangeMaxNegativeY}");
         }
     }
 }
